30e41e51937e20d38342365d1da8488857e772df
joyceeeen <joycenpotestades@yahoo.com>
2017-09-18 09:33:45 +0800

    Update Visitor.php

--- src/Models/Visitor.php
+++ src/Models/Visitor.php
@@ -6,14 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visitor extends Model
 {
-    protected $table = 'kryptonit3_counter_visitor';
-
-    protected $fillable = ['visitor'];
+  protected $table = 'counter_visitor';
+    protected $fillable = ['visitor','client_ip','country','city','latitude','longitude','device','browser','created_at','updated_at'];
     
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
     public $timestamps = false;
 
     public function pages()
     {
-        return $this->belongsToMany('Kryptonit3\Counter\Models\Page', 'kryptonit3_counter_page_visitor', 'visitor_id', 'page_id');
+        return $this->belongsToMany('Kryptonit3\Counter\Models\Page', 'counter_page_visitor', 'visitor_id', 'page_id');
     }
 }
