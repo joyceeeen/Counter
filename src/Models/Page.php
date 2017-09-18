ff2d2ce383b4555c1987e4255f103f20c9b86fcb
joyceeeen <joycenpotestades@yahoo.com>
2017-09-18 09:34:07 +0800

    Update Page.php

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    protected $table = 'kryptonit3_counter_page';
+    protected $table = 'counter_page';
 
-    protected $fillable = ['page'];
-    
+    protected $fillable = ['page','number_of_hit'];
+
     public $timestamps = false;
 
     public function visitors()
     {
-        return $this->belongsToMany('Kryptonit3\Counter\Models\Visitor', 'kryptonit3_counter_page_visitor', 'page_id', 'visitor_id');
+        return $this->belongsToMany('Kryptonit3\Counter\Models\Visitor', 'counter_page_visitor', 'page_id', 'visitor_id');
     }
 }
